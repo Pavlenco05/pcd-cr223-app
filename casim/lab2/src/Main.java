2efa5cf623edb7133365928615ad2c336c0d65aa
Tasio <casim04@mail.ru>
2024-11-05 17:25:59 +0200

    lab3

---
+++ casim/lab2/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class BookStore {
+    public static void main(String[] args) {
+
+        List<List<Object>> orders = List.of(
+                List.of(34587, "Learning Python, Mark Lutz", 4, 40.95),
+                List.of(987620, "Programming Python, Mark Lutz", 5, 56.80),
+                List.of(77226, "Head First Python, Paul Barry", 3, 32.95),
+                List.of(88112, "Einführung in Python3, Bernd Klein", 3, 24.99)
+        );
+
+       }
